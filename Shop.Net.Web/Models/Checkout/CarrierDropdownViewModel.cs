940a7f7c94cc7513532af261ff113837671ede24
Димо Петров <webdude@webdude.eu>
2014-11-13 13:57:32 +0200

    s
    s

---
+++ Shop.Net.Web/Models/Checkout/CarrierDropdownViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Shop.Net.Web.Models.Checkout
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using Shop.Net.Model.Shipping;
+    using Shop.Net.Web.Infrastructure.Mapping;
+
+    public class CarrierDropdownViewModel : IMapFrom<Carrier>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "Money")]
+        public decimal DeliveryPrice { get; set; }
+
+        public int DeliverInDays { get; set; }
+
+        public string Information
+        {
+            get
+            {
+                return string.Format("{0} (Shipping Rate: {1:C}, Delivery in {2} days)", this.Name, this.DeliveryPrice, this.DeliverInDays);
+            }
+        }
+    }
+}
---
+++ Shop.Net.Web/Models/Checkout/ContactInformationDropdownViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Net.Web.Models.Checkout
+{
+    using Shop.Net.Model.Shipping;
+    using Shop.Net.Web.Infrastructure.Mapping;
+
+    public class ContactInformationDropdownViewModel : IMapFrom<ContactInformation>
+    {
+        public int Id { get; set; }
+
+        public string ContactName { get; set; }
+    }
+}
