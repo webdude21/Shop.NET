a13afd9ca02f9e22042dc79d6b7558f998671acd
Димо Петров <webdude@webdude.eu>
2014-11-14 16:16:26 +0200

    s
    s

---
+++ Shop.Net.Web/Areas/Profile/Models/CountryViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Net.Web.Areas.Profile.Models
+{
+    using Shop.Net.Model.Shipping;
+    using Shop.Net.Web.Infrastructure.Mapping;
+
+    public class CountryViewModel : IMapFrom<Country>
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
